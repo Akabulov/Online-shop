7bee16444745813092a0358daadc1d0b3763335a
marat tskhovrebov <116466131+mara-allmighty@users.noreply.github.com>
2025-04-09 23:38:11 +0300

    Update order.py

--- models/order.py
+++ models/order.py
@@ -5,8 +5,8 @@ class Order:
   1.1. order_items: List[Product]
 
   -- Методы --
-  1.2. create_order
-  1.3. save_order
-  1.3. show_history
+  1.2. создать заказ
+  1.3. сохранить заказ
+  1.3. показать историю
   
 """
