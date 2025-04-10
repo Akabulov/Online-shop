bff5a1a48cfa44d88a1e805856058f39ab272c19
marat tskhovrebov <116466131+mara-allmighty@users.noreply.github.com>
2025-04-10 18:51:52 +0300

    Update order.py

--- models/order.py
+++ models/order.py
@@ -12,3 +12,31 @@ class Order:
 
   
 """
+
+
+class Order:
+    # свойства 
+
+    def save_order(self, user: User, filepath: str = "../cart/orders.json"):
+        """ Добавляет заказ в историю заказов пользователя. """
+        
+        if user.username is None:
+            return f"Сначала войдите в систему"
+            
+        try:
+            with open(filepath, "r") as file:
+                data = json.load(file)
+         except FileNotFoundError as e:
+            data = list()
+
+        for item in self.order_items:
+            data.append({"id": item.id, "title": item.title, "price": item.price})
+             
+        try:    
+            with open(filepath, "w") as file:
+                json.dump(data, file, indent=4)
+            return f"Заказ успешно добавлен в историю!"
+        except Exception as e:
+            raise RuntimeError(f"Ошибка при сохранении файла: {e}")
+        
+  
