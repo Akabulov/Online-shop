8b8c4cd2768c41a4630ebbdfa8be04f708b2eed7
Jambolat791 <61623521+Jambolat791@users.noreply.github.com>
2025-04-10 17:35:25 +0300

    Adding password check function
    Добавление функции для проверки надежности пароля

--- utils/helpers.py
+++ utils/helpers.py
@@ -1,1 +1,20 @@
 """  файл для хранения второстепенных функций  """
+def сheck_password(password):
+    symbols_counter = 0
+    upper_letters_counter = 0
+    lower_letters_counter = 0
+    upper_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+    lower_letters = 'abcdefghijklmnopqrstuvwxyz'
+    symbols = '!@#$%^&*()-+'
+    if len(password) < 8:
+        return False
+    for i in password:
+        if i in upper_letters:
+            upper_letters_counter += 1
+        elif i in lower_letters:
+            lower_letters_counter += 1
+        elif i in symbols:
+            symbols_counter += 1
+    if symbols_counter > 0 and lower_letters_counter > 0 and upper_letters_counter > 0:
+        return True
+    return False
